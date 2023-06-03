740fce7f6919d6531ba913b318b92496b42a37c2
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-06-03 10:32:24 +0500

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Ravshanbek Tursunbaev on 2023/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
